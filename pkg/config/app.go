b92e2703027f1f441393bf48a70dd2d6daac9924
piyushk <piyushk@mathworks.com>
2023-10-29 21:23:10 +0530

    running code

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -10,8 +10,9 @@ var (
 )
 
 func Connect(){
-	d, err := gorm.Open("mysql", "piyush:password/mydb?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "piyush:password@tcp(localhost:3306)/mydb?charset=utf8&parseTime=True&loc=Local")
 	// username:password/tablename?<some_inputs_required_by_mysql>
+	// username:password@tcp(hostname:port)/database?charset=utf8mb4&parseTime=True&loc=Local
 	if err != nil{
 		panic(err)
 	}
